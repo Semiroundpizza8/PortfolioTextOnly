fc7bdf1b8d581222f735878ea9410586925abea3
Benjamin Odisho <Semiroundpizza8@gmail.com>
2018-04-02 23:12:47 -0500

    Checkpoint commit

--- server/db/db.js
+++ server/db/db.js
@@ -4,8 +4,8 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  // process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  `postgres://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  // `postgres://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`,
   {
     logging: false
   }
