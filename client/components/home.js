6da8fd443ddada8341a2d7c44f2ee1e2c0e62140
Benjamin Odisho <Semiroundpizza8@gmail.com>
2018-06-01 18:49:11 -0500

    Started redesign

--- client/components/home.js
+++ client/components/home.js
@@ -6,14 +6,17 @@ import { NavLink } from 'react-router-dom';
 export function Home() {
   const textStyle = { display: 'inline' };
   return (
-    <div>
-      <h1 style={textStyle}>Welcome! I’m <NavLink to="/about" onMouseDown={(event) => {event.preventDefault()}}>{overlap('Benjamin Odisho', 'yellow')}</NavLink>, a passionate Chicago-land web developer focused on creating dynamic web applications using <NavLink to="/projects" onMouseDown={(event) => {event.preventDefault()}}>{overlap('Javascript', 'cyan')}</NavLink></h1>
-      <h1 style={textStyle}>Feel free to learn about my background on my <NavLink to="/about" onMouseDown={(event) => {event.preventDefault()}}>{overlap('About', 'pink')}</NavLink> page, get a glimpse into my past projects throught my <NavLink to="/projects" onMouseDown={(event) => {event.preventDefault()}}>{overlap('Projects', 'pink')}</NavLink> page, </h1>
-      <h1 style={textStyle}>and to reach out to me via my <NavLink to="/contact" onMouseDown={(event) => {event.preventDefault()}}>{overlap('Contact', 'pink')}</NavLink> page. </h1>
-      <h1 style={textStyle}>Additionally, if you're still curious about me feel free to visit my <a target="_blank" rel="noopener noreferrer" href='https://github.com/Semiroundpizza8' onMouseDown={(event) => {event.preventDefault()}}>{overlap('Github', 'cyan')}</a> </h1>
-      <h1 style={textStyle}>or <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/benjamin-odisho/" onMouseDown={(event) => {event.preventDefault()}}>{overlap('LinkedIn', 'cyan')}</a> profiles. Thank you for visiting, and hope to speak with you soon!</h1>
-    </div>
+    <h1>Hello!</h1>
   )
 }
 
 export default connect(null, null)(Home);
+
+
+{/* <div>
+<h1 style={textStyle}>Welcome! I’m <NavLink to="/about" onMouseDown={(event) => {event.preventDefault()}}>{overlap('Benjamin Odisho', 'yellow')}</NavLink>, a passionate Chicago-land web developer focused on creating dynamic web applications using <NavLink to="/projects" onMouseDown={(event) => {event.preventDefault()}}>{overlap('Javascript', 'cyan')}</NavLink></h1>
+<h1 style={textStyle}>Feel free to learn about my background on my <NavLink to="/about" onMouseDown={(event) => {event.preventDefault()}}>{overlap('About', 'pink')}</NavLink> page, get a glimpse into my past projects throught my <NavLink to="/projects" onMouseDown={(event) => {event.preventDefault()}}>{overlap('Projects', 'pink')}</NavLink> page, </h1>
+<h1 style={textStyle}>and to reach out to me via my <NavLink to="/contact" onMouseDown={(event) => {event.preventDefault()}}>{overlap('Contact', 'pink')}</NavLink> page. </h1>
+<h1 style={textStyle}>Additionally, if you're still curious about me feel free to visit my <a target="_blank" rel="noopener noreferrer" href='https://github.com/Semiroundpizza8' onMouseDown={(event) => {event.preventDefault()}}>{overlap('Github', 'cyan')}</a> </h1>
+<h1 style={textStyle}>or <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/benjamin-odisho/" onMouseDown={(event) => {event.preventDefault()}}>{overlap('LinkedIn', 'cyan')}</a> profiles. Thank you for visiting, and hope to speak with you soon!</h1>
+</div> */}
